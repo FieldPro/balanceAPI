0e4eccd1ec1894477e9480b3d5fa644e6ad0413b
fieldpro <fasanyapluse@gmail.com>
2020-06-18 18:34:30 +0100

    BalanceAPI

---
+++ script.js
@@ -0,0 +1,19 @@
+  const dataApi = {
+	url: "https://sandbox.wallets.africa/self/balance",
+	WalletBalance:  97900,
+	WalletCurrency: "NGN",
+}
+
+const {url, WalletBalance, WalletCurrency} = dataApi
+
+const urlApi = `${url}` //${WalletBalance}/${WalletCurrency}
+fetch(urlApi)
+	.then( (data) => data.json())
+	.then( (NGN) => html(NGN) )
+
+const html = (data) => {
+	console.log(data)
+	const xhtml = `
+		<div class="name">${data.name}</div>
+		`
+}  
